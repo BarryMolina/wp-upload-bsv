127c77e92e9492aff8e5bddfeb9c2e437d590267
Barry Molina <bazzaboy@gmail.com>
2021-08-22 09:15:52 -0500

    remove markdown converter from gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2bd102976cb6f2ab4f84f385402d850003bfac8e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2bd102976cb6f2ab4f84f385402d850003bfac8e',
+    ),
+    'league/html-to-markdown' => 
+    array (
+      'pretty_version' => '5.0.0',
+      'version' => '5.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4dbebbebe0fe454b6b38e6c683a977615bd7dc2',
+    ),
+  ),
+);
